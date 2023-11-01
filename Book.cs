5e84efedd2718f5739ad5ab613ffa4ae038d4006
bgwhorley <yoshubesan@gmail.com>
2023-11-01 10:22:59 -0400

    added string out parameters, tuples. C# class examples and functionalities

---
+++ Book.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class Book
+    {
+        private string _name;
+        protected string _description;
+        private string _title;
+        private string _author;
+        private int _pagecount;
+
+        public Book() {
+            _name = string.Empty;
+            _description = string.Empty;
+            _title = string.Empty;
+            _author = string.Empty;
+            _pagecount = 0;
+            PageCount = _pagecount;
+        }
+
+
+        // c# has Properties to implement data accessors
+        //
+        public string Name
+        {
+            get {
+                return _name;
+            }
+            set
+            {
+                // value is the keyword used to refer to the value passed to the setter
+                // so in this case it would be Name.
+                _name = value;
+            }
+        }
+
+        public string Author
+        {
+            // => is the shorthand notation for setters and getters
+            get => _author;
+            set => _author = value;
+        }
+
+        // declaring a variable as virtual indicates that a child class may redefine a parent attribute or method
+        public string Description
+        {
+            get => $"{_name} is written by {_author}";
+        }
+
+        public virtual string getDescription()
+        {
+            return $"{Name}, {PageCount}";
+        }
+
+        public int PageCount
+        {
+            // get and set will both 
+            get; set;
+        }
+    }
+
+    public class Book2 : Book
+    {
+        // declare the method to be overridden to redefine the method
+        public override string getDescription()
+        {
+            return $"{Name} by {Author}, {PageCount}";
+        }
+
+        // C# has an implicit ToString() function for every object, but if you want to tailor it
+        // to your own preference you can override it
+        public override string ToString()
+        {
+            return $"Book: {Name} by {Author}";
+        }
+
+        // you can also overload the ToString function
+        public string ToString(string format)
+        {
+            if(format == 'B')
+            {
+                return $"Book: {Name}:{Author}";
+            }
+            if(format == 'F')
+            {
+                return $"Book: {Name} by {Author} is {PageCount} pages;
+            }
+            return ToString();
+        }
+    }
+}
--- CSBasics.cs
+++ CSBasics.cs
@@ -2,12 +2,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CSharpBasics
 {
-    internal class CSBasics
+    class CSBasics
     {
         static void Main(string[] args)
         {
@@ -33,6 +35,55 @@ namespace CSharpBasics
             str.stringBuilderExample();
             str.stringParsing();
 
+            object[] items = { 1, 2, "Hello!", "World", 'X', true, 2.0, ".NET", 'A' };
+            int total = 0;
+            string CountType = "System.String";
+            foreach (object item in items)
+            {
+                if (CountTheType(item, CountType))
+                {
+                    total++;
+                }
+            }
+            Console.WriteLine("items array has " + total + " items of type " + CountType);
+
+            printWithPrefix(prefix: "test", str: "string");
+
+            int sum = 0; int product = 0;
+
+            // not that the out keyword must also be used in the method call
+            PlusTimes(1, 2, out sum, out product);
+
+            Console.WriteLine("sum of 1+2 and product of 1*2: " +  sum + ", " +  product);
+
+            // tuples are grouped values
+            (int a, int b) tupl1 = (5, 10);
+            // modification to the tuple can be done using the . operator
+            tupl1.a = 12;
+            Console.WriteLine(tupl1);
+
+            // a tuple can be any mixutre of types, up to a maximum of 8 elements
+            var tupl2 = ("a string", 55);
+            tupl2.Item1 = "a changed string";
+            Console.WriteLine(tupl2);
+
+            Console.WriteLine(TupleExample(1, 2));
+
+            IsPalindrome("rotoR");
+
+            Book book = new Book();
+            book.PageCount = 100;
+
+            Console.WriteLine(book.PageCount);
+
+            Book2 b2 = new Book2();
+            b2.Name = "Test";
+            b2.Author = "Ben";
+            b2.PageCount = 100;
+
+            Console.WriteLine(b2.getDescription());
+
+            Console.WriteLine("Finished");
         }
 
         public static void executeBasics()
@@ -141,6 +192,80 @@ namespace CSharpBasics
             /// </returns>
         }
 
+        // code challenge for counting data types
+        public static bool CountTheType(object Arg, string TypeToCount)
+        {
+            // Your code goes here. Return true if the type of the Arg is the same
+            // as what the TypeToCount parameter says to count.
+            string[] TypesList = { "int", "string", "bool", "double", "char" };
+            if (Arg.GetType() == null)
+            {
+                return false;
+            }
+            if (Arg.GetType().ToString() == TypeToCount)
+            {
+                return true;
+            }
+            return false;
+        }
+        // setting prefix equal to "" in the parameters sets the default value of prefix
+        // in the function call you can name the specific parameters in any order, but ALL parameters will need to be named
+        public static void printWithPrefix(string str, string prefix = "")
+        {
+            Console.WriteLine($"{prefix} {str}");
+        }
 
+        // out parameters are a mechanism to return multiple values in C#
+        // in the following example, the sum and the product passed into the parameter will be computed in the function
+        // and set to the variable passed in through the call
+        public static void PlusTimes(int arg1, int arg2, out int sum, out int product)
+        {
+            sum = arg1 + arg2;
+            product = arg1 * arg2;
+        }
+
+        public static (int, int)TupleExample(int arg1, int arg2)
+        {
+            return (arg1+1, arg2+2);
+        }
+
+        public static bool IsPalindrome(string str)
+        {
+            try
+            {
+                string s1 = str.ToUpper();
+                s1.Replace(" ", "");
+                Regex r = new Regex(@"\W|_");
+                string s = r.Replace(s1, "");
+                if (s.Length == 0)
+                {
+                    return false;
+                }
+
+                string reverse = "";
+                char[] ch = s.ToCharArray();
+                for (int i = s.Length - 1; i >= 0; i--)
+                {
+                    reverse += ch[i];
+                }
+
+                Console.WriteLine("testing palindrome on strings: " + s + ", " + reverse);
+                if (reverse == s)
+                {
+                    return true;
+                }
+                return false;
+            } 
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("Caught Index out of Range Exception during execution of isPalindrome()");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Caught general exception during execution of IsPalindrome()");
+
+            }
+            return false;
+        }
     }
 }
